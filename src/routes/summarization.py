1d4189863b6d1da04ff1c09fe956af1cbabda349
anhnguyenvv <vvlovesakuravv@gmail.com>
2025-07-21 18:50:26 +0700

    modified:   src/routes/summarization.py

--- src/routes/summarization.py
+++ src/routes/summarization.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify
 from modules.summarization.summarization import summarize_text
 from database.db import save_history
-
+from modules.preprocessing.preprocess import preprocess_text
 summarization_bp = Blueprint('summarization', __name__)
 
 @summarization_bp.route('/summarize', methods=['POST'])
@@ -11,7 +11,7 @@ def summarize():
 
     if not text:
         return jsonify({"error": "No text provided"}), 400
-
+    text = preprocess_text(text, remove_stopwords=True, remove_special_chars=True)
     summary = summarize_text(text)
     if not summary:
         return jsonify({"error": "Failed to summarize text"}), 500
